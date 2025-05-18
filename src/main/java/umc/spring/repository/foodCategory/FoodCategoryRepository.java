ce82bed40cd88604b140a74f512316aa12d171dc
caminobelllo <alstpgml0812@gmail.com>
2025-05-18 20:38:22 +0900

    Feat: FoodCategory repository 생성

---
+++ src/main/java/umc/spring/repository/foodCategory/FoodCategoryRepository.java
@@ -0,0 +1,7 @@
+package umc.spring.repository.foodCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.FoodCategory;
+
+public interface FoodCategoryRepository extends JpaRepository<FoodCategory, Long> {
+}
