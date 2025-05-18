0e7d07ea02773323bffe2ee5ba98621c85de7924
caminobelllo <alstpgml0812@gmail.com>
2025-05-18 18:28:58 +0900

    Feat: Member DTO 작성

---
+++ src/main/java/umc/spring/web/dto/member/MemberRequestDTO.java
@@ -0,0 +1,20 @@
+package umc.spring.web.dto.member;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class MemberRequestDTO {
+
+    @Getter
+    public static class JoinDTO {
+        String name;
+        Integer gender;
+        Integer birthYear;
+        Integer birthMonth;
+        Integer birthDay;
+        String address;
+        String specAddress;
+        List<Long> preferCategory;
+    }
+}
