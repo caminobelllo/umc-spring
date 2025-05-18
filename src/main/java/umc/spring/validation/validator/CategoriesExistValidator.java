4d50afb7970a3e68e0ed7c5a2af99f91ff42cea2
caminobelllo <alstpgml0812@gmail.com>
2025-05-18 20:39:49 +0900

    Feat: category 유효성 검사 로직

---
+++ src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,38 @@
+package umc.spring.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.repository.foodCategory.FoodCategoryRepository;
+import umc.spring.validation.annotation.ExistCategories;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    // 수정
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext constraintValidatorContext) {
+
+        boolean isValid = values.stream()
+                .allMatch(foodCategoryRepository::existsById);
+
+        if (!isValid) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
