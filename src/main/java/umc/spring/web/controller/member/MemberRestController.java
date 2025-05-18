b4a6948e4c0d31a916b35726f6531df0384a24a6
caminobelllo <alstpgml0812@gmail.com>
2025-05-18 18:29:32 +0900

    Feat: Member controller 생성

---
+++ src/main/java/umc/spring/web/controller/member/MemberRestController.java
@@ -0,0 +1,25 @@
+package umc.spring.web.controller.member;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.service.member.MemberCommandService;
+import umc.spring.web.dto.member.MemberRequestDTO;
+import umc.spring.web.dto.member.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDTO request) {
+        return null;
+    }
+}
