7cc32d65c560cc852dd8876f5e8d4e3b3acc5807
caminobelllo <alstpgml0812@gmail.com>
2025-05-18 22:33:24 +0900

    Feat: store controller 구현

---
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -0,0 +1,31 @@
+package umc.spring.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.MemberConverter;
+import umc.spring.converter.StoreConverter;
+import umc.spring.domain.Member;
+import umc.spring.domain.Store;
+import umc.spring.service.store.StoreService;
+import umc.spring.web.dto.store.StoreRequestDTO;
+import umc.spring.web.dto.store.StoreResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreRestController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/")
+    public ApiResponse<StoreResponseDTO.AddStoreResultDTO> addStore(@RequestBody @Valid StoreRequestDTO.AddStoreDTO request) {
+
+        Store store = storeService.addStore(request);
+        return ApiResponse.onSuccess(StoreConverter.toAddStoreResultDTO(store));
+    }
+}
