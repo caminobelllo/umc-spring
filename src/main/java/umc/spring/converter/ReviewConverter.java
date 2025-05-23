fd4e2ba54d9fe0b524db2037ee6a5693ecbe46d5
caminobelllo <alstpgml0812@gmail.com>
2025-05-23 15:50:59 +0900

    Feat: paging 처리용 converter 생성

--- src/main/java/umc/spring/converter/ReviewConverter.java
+++ src/main/java/umc/spring/converter/ReviewConverter.java
@@ -1,11 +1,13 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Review;
 import umc.spring.web.dto.review.ReviewRequestDTO;
 import umc.spring.web.dto.review.ReviewResponseDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReviewConverter {
 
@@ -18,7 +20,7 @@ public class ReviewConverter {
 
     public static Review toReview(ReviewRequestDTO.AddReviewDTO request) {
         return Review.builder()
-                .content(request.getContent())
+                .body(request.getContent())
                 .score(request.getScore())
                 .build();
     }
@@ -32,8 +34,21 @@ public class ReviewConverter {
                 .build();
     }
 
-    public static ReviewResponseDTO.ReviewPreviewListDTO reviewPreviewListDTO(List<Review> reviewList) {
-        return null;
+    public static ReviewResponseDTO.ReviewPreviewListDTO reviewPreviewListDTO(Page<Review> reviewList) {
+
+        // 뱔도의 Review DTO를 제작해 List 생성
+        List<ReviewResponseDTO.ReviewPreviewDTO> reviewPreviewDTOList = reviewList.stream()
+                .map(ReviewConverter::reviewPreviewDTO).toList();
+
+        return ReviewResponseDTO.ReviewPreviewListDTO.builder()
+                .reviewList(reviewPreviewDTOList)
+                .listSize(reviewPreviewDTOList.size())
+                // Page에 있는 메서드들임
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .isFirst(reviewList.isFirst())
+                .isLast(reviewList.isLast())
+                .build();
     }
 
 }
