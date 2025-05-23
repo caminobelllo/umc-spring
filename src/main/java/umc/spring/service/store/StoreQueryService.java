4746f2acbecbeacaabd7d8104755cc2be9ada968
caminobelllo <alstpgml0812@gmail.com>
2025-05-23 15:35:19 +0900

    Feat: service 코드 작성

--- src/main/java/umc/spring/service/store/StoreQueryService.java
+++ src/main/java/umc/spring/service/store/StoreQueryService.java
@@ -1,10 +1,12 @@
 package umc.spring.service.store;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
 import umc.spring.web.dto.review.ReviewResponseDTO;
 
 import java.util.List;
 
 public interface StoreQueryService {
 
-    List<ReviewResponseDTO.ReviewPreviewListDTO> getReviewList(Long storeId, Integer page);
+    Page<Review> getReviewList(Long storeId, Integer page);
 }
--- src/main/java/umc/spring/service/store/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/store/StoreQueryServiceImpl.java
@@ -1,15 +1,28 @@
 package umc.spring.service.store;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import umc.spring.web.dto.review.ReviewResponseDTO;
+import umc.spring.domain.Review;
+import umc.spring.domain.Store;
+import umc.spring.repository.review.ReviewRepository;
+import umc.spring.repository.store.StoreRepository;
 
-import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StoreQueryServiceImpl implements StoreQueryService{
 
+    private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
+
     @Override
-    public List<ReviewResponseDTO.ReviewPreviewListDTO> getReviewList(Long storeId, Integer page) {
-        return null;
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+
+        Store store = storeRepository.findById(storeId).get();
+
+        return reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
     }
+
 }
