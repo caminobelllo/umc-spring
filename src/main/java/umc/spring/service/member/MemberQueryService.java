8ec68081457298f768f84fb04219284ddedd653b
caminobelllo <alstpgml0812@gmail.com>
2025-05-23 17:26:14 +0900

    Feat: service 코드 구현

--- src/main/java/umc/spring/service/member/MemberQueryService.java
+++ src/main/java/umc/spring/service/member/MemberQueryService.java
@@ -1,4 +1,10 @@
 package umc.spring.service.member;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Member;
+import umc.spring.domain.Review;
+
 public interface MemberQueryService {
+
+    Page<Review> getReviewList(Long memberId, Integer page);
 }
