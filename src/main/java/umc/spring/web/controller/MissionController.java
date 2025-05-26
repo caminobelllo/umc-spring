a98e06b0b187e46ab03e7baa88d9e381d03cc087
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 14:34:20 +0900

    Rename: 파일 위치 변경

--- src/main/java/umc/spring/web/controller/MissionController.java
+++ src/main/java/umc/spring/web/controller/MissionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MissionConverter;
 import umc.spring.domain.Mission;
-import umc.spring.service.mission.MissionChallengeService;
+import umc.spring.service.mission.MemberMissionService;
 import umc.spring.service.mission.MissionService;
 import umc.spring.web.dto.mission.MissionRequestDTO;
 import umc.spring.web.dto.mission.MissionResponseDTO;
@@ -21,7 +21,7 @@ import umc.spring.web.dto.memberMission.MissionChallengeRequestDTO;
 public class MissionController {
 
     private final MissionService missionService;
-    private final MissionChallengeService missionChallengeService;
+    private final MemberMissionService missionChallengeService;
 
     // 미션 추가하는 API
     @Operation(summary = "미션 추가하기", description = "특정 가게에 미션을 추가합니다.")
