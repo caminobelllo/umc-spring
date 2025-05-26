12bca99815b8bdea45fe1eb239ba5e7d73fcd95a
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 14:34:40 +0900

    Feat: service단 구현

--- src/main/java/umc/spring/service/mission/MemberMissionService.java
+++ src/main/java/umc/spring/service/mission/MemberMissionService.java
@@ -1,9 +1,14 @@
 package umc.spring.service.mission;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import umc.spring.domain.mapping.MemberMission;
+import umc.spring.web.dto.memberMission.MemberMissionResponseDTO;
 import umc.spring.web.dto.memberMission.MissionChallengeRequestDTO;
 
 public interface MemberMissionService {
     void challengeMission(MissionChallengeRequestDTO.ChallengeMissionDTO request);
 
+    Page<MemberMission> getInProgressMissionList(Long memberId, Integer page);
 
 }
--- src/main/java/umc/spring/service/mission/MemberMissionServiceImpl.java
+++ src/main/java/umc/spring/service/mission/MemberMissionServiceImpl.java
@@ -1,6 +1,8 @@
 package umc.spring.service.mission;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
@@ -8,6 +10,7 @@ import umc.spring.apiPayload.exception.handler.CustomErrorHandler;
 import umc.spring.converter.MemberMissionConverter;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.repository.member.MemberRepository;
 import umc.spring.repository.mission.MemberMissionRepository;
@@ -34,4 +37,16 @@ public class MemberMissionServiceImpl implements MemberMissionService {
         MemberMission memberMission = MemberMissionConverter.toMemberMission(request, member, mission);
         memberMissionRepository.save(memberMission);
     }
+
+    @Override
+    public Page<MemberMission> getInProgressMissionList(Long memberId, Integer page) {
+        Member member = memberRepository.findById(memberId).get();
+        // 멤버 없는 경우 에러 처리하기
+        return memberMissionRepository.findAllByMemberAndStatus(
+                member,
+                MissionStatus.CHALLENGING,
+                PageRequest.of(page, 10));
+    }
+
+
 }
