0cf3111d4af7d31a77f4d48824d01b46adcac5e8
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 13:37:57 +0900

    Feat: controller 구현

--- src/main/java/umc/spring/web/controller/StoreRestController.java
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -1,19 +1,28 @@
 package umc.spring.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
+import umc.spring.converter.MissionConverter;
 import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Store;
+import umc.spring.service.mission.MissionService;
+import umc.spring.service.store.StoreQueryService;
 import umc.spring.service.store.StoreService;
+import umc.spring.validation.annotation.ExistStore;
+import umc.spring.web.dto.mission.MissionResponseDTO;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreResponseDTO;
 
@@ -24,6 +33,8 @@ import umc.spring.web.dto.store.StoreResponseDTO;
 public class StoreRestController {
 
     private final StoreService storeService;
+    private final MissionService missionService;
+    private final StoreQueryService storeQueryService;
 
     // 가게 추가하는 API
     @Operation(summary = "가게 추가하기", description = "특정 지역에 가게를 추가합니다.")
@@ -34,5 +45,21 @@ public class StoreRestController {
         return ApiResponse.onSuccess(StoreConverter.toAddStoreResultDTO(store));
     }
 
+    // 가게의 미션을 조회하는 API
+    @Operation(summary = "특정 가게의 미션 조회하기", description = "특정 가게의 미션 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @GetMapping("/list/mission/{storeId}")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access token 필요", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access token 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access token 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다.")
+    })
+    public ApiResponse<MissionResponseDTO.MissionPreviewListDTO> getStoreMissions(@ExistStore @PathVariable(name = "storeId") Long storeId, @RequestParam(name = "page") Integer page) {
+        Page<Mission> missionList = storeQueryService.getMissionList(storeId, page);
+        return ApiResponse.onSuccess(MissionConverter.missionPreviewListDTO(missionList));
+    }
 
 }
