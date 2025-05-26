a75a94b5071fde56d14abb292ef546ade3657844
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 11:46:17 +0900

    Feat: DTO 정의

--- src/main/java/umc/spring/web/dto/mission/MissionResponseDTO.java
+++ src/main/java/umc/spring/web/dto/mission/MissionResponseDTO.java
@@ -3,10 +3,9 @@ package umc.spring.web.dto.mission;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 
 public class MissionResponseDTO {
@@ -15,10 +14,31 @@ public class MissionResponseDTO {
     @Builder
     @AllArgsConstructor
     public static class AddMissionResultDTO {
-
         private Long missionId;
         private Integer reward;
         private String missionSpec;
         private LocalDateTime createdAt;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class MissionPreviewDTO {
+        Integer reward;
+        String missionSpec;
+        Period dDay;
+        Long storeId;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class MissionPreviewListDTO {
+        List<MissionResponseDTO.MissionPreviewDTO> missionList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
     }
 }
