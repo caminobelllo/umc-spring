e8318de4d8dbef044dab51626aa67135c6beabbe
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 14:33:29 +0900

    Feat: dto 추가 구현 (page, mission status)

--- src/main/java/umc/spring/web/dto/memberMission/MemberMissionResponseDTO.java
+++ src/main/java/umc/spring/web/dto/memberMission/MemberMissionResponseDTO.java
@@ -3,6 +3,10 @@ package umc.spring.web.dto.memberMission;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.web.dto.mission.MissionResponseDTO;
+
+import java.util.List;
 
 public class MemberMissionResponseDTO {
 
@@ -12,8 +16,20 @@ public class MemberMissionResponseDTO {
     public static class MissionInProgressDTO {
         Integer reward;
         String missionSpec;
-        String missionStatus;
+        MissionStatus missionStatus;
         String storeName;
         Long missionId;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class MemberMissionListDTO {
+        List<MemberMissionResponseDTO.MissionInProgressDTO> memberMissionList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
     }
 }
