b48d9b3696645f96c735ae0ac436e3915097099c
caminobelllo <alstpgml0812@gmail.com>
2025-05-26 11:46:29 +0900

    Feat: converter 정의

--- src/main/java/umc/spring/converter/MissionConverter.java
+++ src/main/java/umc/spring/converter/MissionConverter.java
@@ -1,11 +1,16 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
 import umc.spring.web.dto.mission.MissionRequestDTO;
 import umc.spring.web.dto.mission.MissionResponseDTO;
+import umc.spring.web.dto.review.ReviewResponseDTO;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.List;
 
 public class MissionConverter {
 
@@ -23,6 +28,32 @@ public class MissionConverter {
                 .reward(request.getReward())
                 .deadline(request.getDeadline())
                 .missionSpec(request.getMissionSpec())
+                .build();
+    }
+
+    // 미션 조회
+    public static MissionResponseDTO.MissionPreviewDTO missionPreviewDTO(Mission mission) {
+        return MissionResponseDTO.MissionPreviewDTO.builder()
+                .reward(mission.getReward())
+                .missionSpec(mission.getMissionSpec())
+                .dDay(Period.between(LocalDate.now(), mission.getDeadline()))
+                .storeId(mission.getStore() != null ? mission.getStore().getId() : null)
+                .build();
+    }
+    public static MissionResponseDTO.MissionPreviewListDTO missionPreviewListDTO(Page<Mission> missionList) {
+
+        List<MissionResponseDTO.MissionPreviewDTO> missionPreviewDTOList = missionList.stream()
+                .map(MissionConverter::missionPreviewDTO)
+                .toList();
+
+        return MissionResponseDTO.MissionPreviewListDTO.builder()
+                .missionList(missionPreviewDTOList)
+                .listSize(missionPreviewDTOList.size())
+                // Page에 있는 메서드들임
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .isFirst(missionList.isFirst())
+                .isLast(missionList.isLast())
                 .build();
     }
 }
