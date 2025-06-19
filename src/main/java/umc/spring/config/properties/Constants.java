93207d12b1ff494549b32da2cb7a0f18b4296e9e
caminobelllo <alstpgml0812@gmail.com>
2025-06-19 14:54:05 +0900

    Feat: properties 설정 클래스

---
+++ src/main/java/umc/spring/config/properties/Constants.java
@@ -0,0 +1,7 @@
+package umc.spring.config.properties;
+
+public class Constants {
+
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
